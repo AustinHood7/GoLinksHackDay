cb1f594268476efbd0c5a176e7957dba783c3158
austin.hood <Austinmhood@live.com>
2023-05-30 15:38:53 -0400

    inital commit

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,126 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { useState, useEffect } from 'react'
+import { Roboto } from 'next/font/google';
+import axios from 'axios';
+ 
 
 export default function Home() {
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [commits, setCommits] = useState([]);
+  const [selectedRepo, setSelectedRepo] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    //this will fetch data in a loop until it returns an empty array
+    const fetchData = async () => {
+      let page = 1;
+      let fetchedRepos = [];
+      while (true) {
+        try {
+          const result = await axios.get(
+            'https://api.github.com/search/repositories', {
+              params: {
+                q: 'user:netflix',
+                sort: 'stars',
+                order: 'desc',
+                page: page,
+              },
+            }
+          );
+
+          //here is what will exit the loop
+          if (result.data.items.length === 0) {
+            break;
+          }
+
+          fetchedRepos = [...fetchedRepos, ...result.data.items];
+          page++;
+        } catch (error) {
+          console.error("Error fetching data", error);
+          break;
+        }
+      }
+      
+      setRepos(fetchedRepos);
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  const fetchCommits = async (repoName) => {
+    try {
+      const result = await axios.get(
+        `https://api.github.com/repos/netflix/${repoName}/commits`
+      );
+
+      setCommits(result.data);
+    } catch (error) {
+      console.error("Error fetching commits", error);
+    }
+  };
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+    console.log("setIsOpen")
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='flex flex-col items-center pt-10 bg-slate-900 text-sky-100'>
+      <h1 className='text-4xl'>Welcome to my GoLinks FrontEnd Project</h1>
+      <h2 className='text-3xl text-sky-300 mt-5'>Author: Austin Hood</h2>
+      <ul className=''>
+        {repos.map((repo) => (
+          <div className='border rounded-xl my-10 max-w-[50%] mx-auto px-10 flex'>
+            <div className=''>
+              <li key={repo.id} className='my-5'>
+                <h2 className='text-2xl pb-5'>{repo.name}</h2>
+                <p className='pb-2'>{repo.description}</p>
+                <p className='pb-2'>Language: {repo.language}</p>
+                <p className='pb-2'> Stars: {repo.stargazers_count}</p>
+                <p className='pb-2'>Forks: {repo.forks_count}</p>
+                <p className='mb-5'>Created at: {new Date(repo.created_at).toLocaleDateString()}</p>
+                <button onClick={() => {
+                  setSelectedRepo(repo.name);
+                  fetchCommits(repo.name);
+                  toggleDropdown();
+                }} className='border-0 rounded-xl bg-sky-300 text-slate-900 p-3'>
+                  Show Commits
+                </button>
+                
+              </li>
+            </div>
+            
+            {isOpen && (
+        <div className="my-5 rounded-md shadow-lg bg-sky-300 text-slate-900 px-5 overflow-y-scroll max-h-[30rem] ml-[250px]">
+          <h1 className="text-3xl font-bold my-4">Commits for {selectedRepo}</h1>
+          <ul>
+            {commits.map((commit) => (
+              <li key={commit.sha} className="mb-4 p-4 border rounded-xl border-slate-900 ">
+                <p className="font-bold">Message: {commit.commit.message}</p>
+                <p>Committer: {commit.commit.committer.name}</p>
+                <p className='max-w-[25%]'>Hash: {commit.sha}</p>
+                <p>Date: {new Date(commit.commit.committer.date).toLocaleDateString()}</p>
+              
+              </li>
+              
+            ))}
+          </ul>
         </div>
-      </div>
+      )}
+          </div>
+          
+        ))}
+        
+      </ul>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    </div>
+  );
+};
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
